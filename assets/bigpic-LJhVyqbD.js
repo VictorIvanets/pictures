710352607d17369b3e54960102bc4dcf8863380a
unknown <imperia.zt@gmail.com>
2024-08-09 11:52:51 +0300

    Updates

---
+++ assets/bigpic-LJhVyqbD.js
@@ -0,0 +1,1 @@
+import{b as e,g as o,j as i}from"./index-ClFNpD6u.js";const t=()=>{const{id:s}=e(),c=o();return i.jsx("div",{onClick:a=>{c(-1)},onKeyDown:a=>console.log(a),className:"bigpic",children:i.jsx("div",{className:"bigpic__picbox",children:i.jsx("img",{className:"bigpic__pic",src:`/pictures/bigl_${s}.jpg`,alt:`bigl_${s}.jpg`})})})};export{t as default};
