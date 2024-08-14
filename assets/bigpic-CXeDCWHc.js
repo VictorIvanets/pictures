b7d82d7832f61ab0a699988c60cddc3a63d0946a
unknown <imperia.zt@gmail.com>
2024-08-14 14:58:01 +0300

    Updates

--- assets/bigpic-CXeDCWHc.js
+++
@@ -1,1 +0,0 @@
-import{b as e,g as o,j as i}from"./index-g2L4G2Ml.js";const t=()=>{const{id:s}=e(),c=o();return i.jsx("div",{onClick:a=>{c(-1)},onKeyDown:a=>console.log(a),className:"bigpic",children:i.jsx("div",{className:"bigpic__picbox",children:i.jsx("img",{className:"bigpic__pic",src:`/pictures/bigl_${s}.jpg`,alt:`bigl_${s}.jpg`})})})};export{t as default};
